339547874b6281b9e497e20f38dc4f87ebe2d11f
CostaTech <ilprocostantino@gmail.com>
2025-10-31 12:37:29 +0100

    Create terextsocial(incomplete).py

---
+++ terextsocial(incomplete).py
@@ -0,0 +1,34 @@
+
+
+
+print("""
+████████╗███████╗██████╗ ███████╗██╗  ██╗████████╗
+╚══██╔══╝██╔════╝██╔══██╗██╔════╝╚██╗██╔╝╚══██╔══╝
+   ██║   █████╗  ██████╔╝█████╗   ╚███╔╝    ██║   
+   ██║   ██╔══╝  ██╔══██╗██╔══╝   ██╔██╗    ██║   
+   ██║   ███████╗██║  ██║███████╗██╔╝ ██╗   ██║   
+   ╚═╝   ╚══════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝   ╚═╝   
+                                                  
+""")
+
+
+
+
+print(“Hello, please enter your name to register (this information will not be made public)”)
+username = input(‘Enter your name: ’)
+print(username)
+print(“Okay, now enter your surname below (this information will not be made public)”)
+Surname = input(“Enter your surname: ”)
+print(Surname)
+print(“Ok, now enter your nickname. This name will be made public, so don't use your real name”)
+Nickname = input(“Enter your Nickname: ”)
+print(Nickname)
+print(“Ok, perfect. Now enter your Gmail address (this information will not be made public”)
+Gmail = input(“Enter your Gmail: ”)
+print(Gmail)
+print(“Last thing, your password. This information will not be made public”)
+Password = input(“Enter your Password: ”)
+print(Password)
+print(“Perfect! Your account has been successfully created”)
+input(“”)
+      
